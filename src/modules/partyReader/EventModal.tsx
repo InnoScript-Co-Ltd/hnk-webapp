fdf799502f35cd768277ca57797aeab664f95c99
aunghp <aunghp.io@gmail.com>
2024-04-02 09:07:08 +0630

    fix

--- src/modules/partyReader/EventModal.tsx
+++ src/modules/partyReader/EventModal.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
 import './eventModalStyle.css'
 
 export default function EventModal({description, onClose}:any) {
   return (
     <div className="w-full h-full fixed top-0 left-0 z-[9999] backdrop-blur-sm bg-white/30">
       <div className="modal-detail-container overflow-hidden">
-        <div className="modal-detail-content">
-          
+        <div className="modal-detail-content" style={{padding: "20px"}}>
           <p className="modal-title">Event Detail</p>
           <div className="modal-content">
               <div className="modal-item-value"> {description} </div>
